c8b2aa1cfa664fb902a7210132e8bd64b9febcdf
sznorbee <norbert.szekeres@gmail.com>
2018-02-27 12:39:54 +0100

    php exercise 2 ok

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,15 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+$middlePasswordLength = ceil((strlen($password))/2); //5/2= 2.5 the position at insert celi to have an integer 3
+
+//to have the second half of password
+$lastHalfPassword = substr($password, $middlePasswordLength);
+
+//first part + salt = replace the last part with the salt
+$firstPartPlusSalt = str_replace($lastHalfPassword, $salt, $password);
+
+//concatinaite the firstpart plus salt with the last part
+$saltedPassword = $firstPartPlusSalt.$lastHalfPassword;
+
+echo $saltedPassword;
