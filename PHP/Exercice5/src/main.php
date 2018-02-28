a34dbfbd7aa9cdb70194571a64b6fa88582eab5f
sznorbee <norbert.szekeres@gmail.com>
2018-02-28 12:35:30 +0100

    again ex5 and ...

--- PHP/Exercice5/src/main.php
+++ PHP/Exercice5/src/main.php
@@ -4,11 +4,11 @@ require_once __DIR__.'/DimensionalMath/Distance/DistanceCalculation.php';
 require_once __DIR__.'/DimensionalMath/Vector/AngleCalculation.php';
 
 use function DimensionalMath\Distance\threeDimensionDistance;
-use function DimensionalMath\Vector\getVectorAngle;
+use function DimensionalMath\Vector\getVectorAngle as vectorAngle;
 
 $distance = threeDimensionDistance(
     [1, 1, 1],
     [2, 2, 2]
 );
 
-$angle = getVectorAngle([1, 6], [3, 12]);
+$angle = vectorAngle([1, 6], [3, 12]);
