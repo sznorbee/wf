d6988bc6e60ff60e545502fbc5b62a604cc431f8
sznorbee <norbert.szekeres@gmail.com>
2018-03-01 17:02:55 +0100

    ex7 not finished at17h

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,23 @@
 <?php
+
+//create a function with two parameter
+function divide($number, $divisor ){
+    
+    if ($divisor == 0){
+        throw new RuntimeException("Divison by 0!!!");
+    }
+    return $result = $number / $divisor;
+}
+
+function arrayDivide($array, $divisor){
+    
+    if ($divisor == 0){
+        throw new RuntimeException("Divison by 0!!!");
+    } 
+}
+
+try {
+    arrayDivide($value, 0);
+}catch (RuntimeException $catcher){
+    return $value;
+}
